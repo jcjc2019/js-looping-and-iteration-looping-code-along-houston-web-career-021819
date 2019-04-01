5cf0fe4ec6897ef36e2e0fdb29eb5bc6cad9537c
JingChen <jingchenjc2019@gmail.com>
2019-04-01 15:01:04 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solutions in this file
+//for loop
+function printBadges(arr){
+    for (let i = 0; i < arr.length; i++){
+        console.log(`Welcome ${arr[i]}! You are employee #${i+1}.`);
+    }
+    return arr;
+};
+printBadges(['Ada', 'Brendan', 'Ali']);
+
+//while loop
+
+function tailsNeverFails() {
+    let x = 0;
+    while (Math.random() >= 0.5) {
+        x++
+    }
+    return `You got ${x} tails in a row!`;
+};
